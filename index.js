2e33dfea36997d3872ba673e8d264fd9ae04fecc
httpjml <joannamleonardo@gmail.com>
2024-03-10 22:18:52 -0400

    API Integration

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+
+
+    cityElement.innerHTML = response.data.city
+    temperatureElement.innerHTML = Math.round(temperature);
+}
+
+
+
+function searchCity(city) {
+
+    let apiKey = "f38e493t0d8cf873o54b6bb38aa6cc29";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`
+  axios.get(apiUrl).then(refreshWeather)
+}
+
+
 
 function handleSearchSubmit (event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+
+  
+    searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form")
 searchFormElement.addEventListener ("submit", handleSearchSubmit);
+
+searchCity("Philadelphia")
