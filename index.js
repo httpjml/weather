95f783f30f8d0fa1f492a6f9e1ea08b365974323
httpjml <joannamleonardo@gmail.com>
2024-03-23 21:33:48 -0400

    injected forecast HTML to JS
    1. removed html from html file and added id forecast to existing weather forecast.
    2. called function displayForecast
    created array of 5 days
    
    3. created a string forecast Html through a loop of html

--- index.js
+++ index.js
@@ -64,7 +64,37 @@ function handleSearchSubmit (event) {
     searchCity(searchInput.value);
 }
 
+function displayForecast() {
+ 
+
+    let days = ["Tues", "Wed", "Thu", "Fri", "Sat", "Sun"];
+    let forecastHtml = "";
+
+    days.forEach (function(day){
+        forecastHtml = 
+        forecastHtml + 
+        `
+        <div class="weather-forecast-day">
+            <div class="weather-forecast-date"> ${day} </div>
+            <div class="weather-forecast-icon">🌤</div>
+            <div class="weather-forecast-temperatures">
+                <div class="weather-forecast-temperature"> 
+                <strong>18°</strong>
+             </div>
+        <div class="weather-forecast-temperature"> 12° </div>
+    </div>
+</div>
+   `;
+
+    });
+    let forecastElement = document.querySelector("#forecast");
+    forecastElement.innerHTML = forecastHtml;
+
+}
+
 let searchFormElement = document.querySelector("#search-form")
 searchFormElement.addEventListener ("submit", handleSearchSubmit);
 
-searchCity("Philadelphia")
+searchCity("Philadelphia");
+
+displayForecast();
